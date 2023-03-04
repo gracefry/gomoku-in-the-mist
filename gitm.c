b13b49de09e89dadb7747ef11fb35a416707fe5a
Grace <99232016+gracefry@users.noreply.github.com>
2023-03-04 23:49:14 +1100

    Refactor term and who as functions

--- gitm.c
+++ gitm.c
@@ -4,21 +4,44 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
+void term() {
+    exit(1);
+}
+
+char who(int turn) {
+    char player;
+
+    if (turn % 2 == 0) {
+        player = 'B';
+    } else {
+        player = 'W';
+    }
+
+    printf("%c\n", player);
+    return player;
+}
 
 int main(int argc, char* argv[]) {
     char command[256];
     int game_over = 0;
+    int turn = 0;
 
     while (game_over == 0) {
         fgets(command, 256, stdin);
 
-        // term command
+        // command switcher
         if (!strcmp(command, "term\n")) {
-            return 1;
+            term();
+        } else if (!strcmp(command, "who\n")) {
+            who(turn);
+        } else {
+            printf("Invalid!\n");
         }
 
-        printf("%s", command);
-        game_over = 1;
+        // printf("%s", command);
+        // game_over = 1;
     }
 
     return 0;
