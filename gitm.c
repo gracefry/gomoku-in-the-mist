38fc56f2b1e0476dacdcf278c0b6bba53de094e3
Grace <99232016+gracefry@users.noreply.github.com>
2023-03-09 05:16:46 +1100

    Move tests to diff folder, add place fx

--- gitm.c
+++ gitm.c
@@ -6,11 +6,73 @@
 #include <string.h>
 #include <stdlib.h>
 
+char board[20][20];
+char command[256];
+int game_over = 0;
+int turn = 0;
+
+void makeBoard() {
+    for (int i = 0; i <= 20; i++) {
+        for (int j = 0; j <= 20; j++) {
+            board[i][j] = '.';
+        }
+    }
+}
+
+int place(char c, int r, char player) {
+    int num_c = c - 'A' + 1;
+
+    // Check column and row validity
+    if ((num_c < 1 || num_c > 19) || (r < 1 || r > 19)) {
+        printf("Invalid coordinate");
+        // printf("inval %d, %d", num_c, r);
+        return 1;
+    } 
+
+    // Check occupancy
+    if (board[num_c][r] != '.') {
+        printf("Occupied coordinate");
+        return 1;
+    }
+    
+    // Place according to player
+    if (player == 'B') {
+        board[num_c][r] = '#';
+        // printf("black %d, %d", num_c, r);
+    } else {
+        board[num_c][r] = 'o';
+    }
+
+    turn++;
+    return 0;
+}
+
 void term() {
+    /*
+    * Function:  term
+    * --------------------
+    * Forces the controller to terminate with exit code 1. 
+    * This command can be executed at any time. 
+    * This command will suppress history moves messages and the exit message.
+    */
+
     exit(1);
 }
 
 char who(int turn) {
+    /*
+    * Function:  who 
+    * --------------------
+    * Shows who is the current player. 
+    * This command can be executed at any time.
+    *
+    *   turn: integer value of the current turn 
+    *       turn increments by 1 for every valid place command
+    *
+    *   returns: 
+    *       char 'B' for black, or
+    *       char 'W' for white
+    */
     char player;
 
     if (turn % 2 == 0) {
@@ -19,28 +81,37 @@ char who(int turn) {
         player = 'W';
     }
 
-    printf("%c\n", player);
     return player;
 }
 
 int main(int argc, char* argv[]) {
-    char command[256];
-    int game_over = 0;
-    int turn = 0;
+    makeBoard();
 
     while (game_over == 0) {
         fgets(command, 256, stdin);
+        char player = who(turn);
 
         // command switcher
-        if (!strcmp(command, "term\n")) {
+        if (!strncmp(command, "term", 4)) {
             term();
-        } else if (!strcmp(command, "who\n")) {
-            who(turn);
+        } else if (!strncmp(command, "who", 3)) {
+            printf("%c", player);
+        } else if (!strncmp(command, "place", 5)) {
+            char c = command[6];
+            int r = 0;
+
+            for (int i = 7; command[i] != '\0'; i++) {
+                if (command[i] <= '9' && command[i] >= '0') {
+                r = r * 10 + command[i] - '0';
+                }
+            }
+
+            place(c, r, player);
+
         } else {
             printf("Invalid!\n");
         }
-
-        // printf("%s", command);
+        printf("\n");
         // game_over = 1;
     }
 
